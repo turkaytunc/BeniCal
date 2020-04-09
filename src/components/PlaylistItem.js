b6aecaa938b6e629e2e044ec94b810fe550b2ce0
Turkay Tunc <turkaytunc00@gmail.com>
2020-04-09 14:07:32 +0300

    Update Styled components

--- src/components/PlaylistItem.js
+++ src/components/PlaylistItem.js
@@ -4,7 +4,9 @@ import StyledPlaylistItem from "./styles/StyledPlaylistItem";
 const PlaylistItem = ({ video, active, played }) => {
   return (
     <StyledPlaylistItem active={active} played={played}>
-      PlaylistItem
+      <div className="tt-player__video-nr">{video.num}</div>
+      <div className="tt-player__video-name">{video.title}</div>
+      <div className="tt-player__video-time">{video.duration}</div>
     </StyledPlaylistItem>
   );
 };
--- src/components/styles/StyledPlaylistItem.js
+++ src/components/styles/StyledPlaylistItem.js
@@ -5,9 +5,9 @@ const StyledPlaylistItem = styled.div`
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   background: none;
-  border: ${props => (props.active ? "2px dotted #696969" : "none")};
+  border: ${(props) => (props.active ? "2px dotted #696969" : "none")};
   font-size: 1.6em;
-  color: ${props => props.theme.color};
+  color: ${(props) => props.theme.color};
   min-height: 50px;
   padding: 10px 20px;
   margin: 0 0 10px 0;
@@ -17,12 +17,12 @@ const StyledPlaylistItem = styled.div`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  border-radius: ${props => (props.active ? "5px" : "0")};
+  border-radius: ${(props) => (props.active ? "5px" : "0")};
   cursor: pointer;
   position: relative;
 
   ::before {
-    display: ${props => (props.active || !props.played ? "none" : "block")};
+    display: ${(props) => (props.active || !props.played ? "none" : "block")};
     content: "";
     width: 10px;
     min-height: 10px;
@@ -33,24 +33,24 @@ const StyledPlaylistItem = styled.div`
     top: 19px;
   }
 
-  .wbn-player__video-nr {
+  .tt-player__video-nr {
     flex-grow: 1; /* default 0 */
     max-width: 30px;
     padding-right: 20px;
     border-right: 2px solid #565656;
   }
 
-  .wbn-player__video-name,
-  .wbn-player__video-name a {
+  .tt-player__video-name,
+  .tt-player__video-name a {
     flex-grow: 10; /* default 0 */
     padding: 0 0 0 10px;
-    color: ${props => props.theme.color};
+    color: ${(props) => props.theme.color};
     text-align: left;
     text-decoration: none;
     display: block;
   }
 
-  .wbn-player__video-time {
+  .tt-player__video-time {
     background: #565656;
     min-width: 45px;
     color: #fff;
@@ -62,9 +62,9 @@ const StyledPlaylistItem = styled.div`
     margin: 0 0 0 5px;
   }
 
-  .wbn-player__video-download {
+  .tt-player__video-download {
     flex-grow: 1; /* default 0 */
-    color: ${props => (props.played ? "#fff" : props.theme.color)};
+    color: ${(props) => (props.played ? "#fff" : props.theme.color)};
     margin: 0 0 0 20px;
     max-width: 30px;
     padding: 0 20px;
@@ -74,8 +74,8 @@ const StyledPlaylistItem = styled.div`
     font-weight: normal;
   }
 
-  .wbn-player__video-download a {
-    color: ${props =>
+  .tt-player__video-download a {
+    color: ${(props) =>
       props.played && !props.active ? "#fff" : props.theme.color};
   }
 
