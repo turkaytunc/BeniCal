fe28e9b9c6a1b7a74308a553149ade609b2c43fd
Turkay Tunc <turkaytunc00@gmail.com>
2020-04-09 14:15:21 +0300

    Update components

--- src/components/containers/Playlist.js
+++ src/components/containers/Playlist.js
@@ -4,13 +4,16 @@ import PlaylistItems from "../containers/PlaylistItems";
 import Nightmode from "../Nightmode";
 import StyledPlaylist from "../styles/StyledPlaylist";
 
-const Playlist = (props) => {
+const Playlist = ({ videos, active, nightModeCallback, nightMode }) => {
   return (
     <>
       <StyledPlaylist>
-        <Nightmode></Nightmode>
-        <PlaylistHeader></PlaylistHeader>
-        <PlaylistItems></PlaylistItems>
+        <Nightmode
+          nigtModeCallback={nightModeCallback}
+          nightMode={nightMode}
+        ></Nightmode>
+        <PlaylistHeader active={active} total={videos.length}></PlaylistHeader>
+        <PlaylistItems video={videos} active={active}></PlaylistItems>
       </StyledPlaylist>
     </>
   );
