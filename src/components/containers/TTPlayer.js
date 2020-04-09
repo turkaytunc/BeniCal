97a91d8197b1b71d8dcfae3a2490e6aa1032ccee
Turkay Tunc <turkaytunc00@gmail.com>
2020-04-09 13:48:28 +0300

    Imported Styled components

--- src/components/containers/TTPlayer.js
+++ src/components/containers/TTPlayer.js
@@ -1,13 +1,36 @@
 import React from "react";
 import Video from "../Video";
 import Playlist from "../containers/Playlist";
+import { ThemeProvider } from "styled-components";
+
+const darkTheme = {
+  bgcolor: "#353535",
+  bgcolorItem: "#414141",
+  bgcolorItemActive: "#405c63",
+  bgcolorPlayed: "#526d4e",
+  border: "none",
+  borderPlayed: "none",
+  color: "#fff",
+};
+
+const lightTheme = {
+  bgcolor: "#fff",
+  bgcolorItem: "#fff",
+  bgcolorItemActive: "#80a7b1",
+  bgcolorPlayed: "#7d9979",
+  border: "1px solid #353535",
+  borderPlayed: "none",
+  color: "#353535",
+};
 
 const TTPlayer = () => {
   return (
-    <>
-      <Video></Video>
-      <Playlist></Playlist>
-    </>
+    <ThemeProvider theme={state.nightMode ? darkTheme : lightTheme}>
+      <>
+        <Video></Video>
+        <Playlist></Playlist>
+      </>
+    </ThemeProvider>
   );
 };
 
