b8a6c9146d93ec244a24be84a1a91eb877749a5d
Turkay Tunc <turkaytunc00@gmail.com>
2020-04-09 13:18:25 +0300

    Added styled components

--- src/components/containers/TTPlayer.js
+++ src/components/containers/TTPlayer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Video from "../Video";
 import Playlist from "../containers/Playlist";
+import StyledComponents from "styled-components";
 
 const TTPlayer = () => {
   return (
